161fd58a22fb0aa590430a9d291954ccae66d7a7
Jlius Suweno <jliuss@live.com>
2020-10-08 08:36:43 +0700

    Initial Commit

---
+++ DayBIT14Hybrid/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  DayBIT14Hybrid
+//
+//  Created by Jlius Suweno on 08/10/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ DayBIT14Hybrid/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  DayBIT14Hybrid
+//
+//  Created by Jlius Suweno on 08/10/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
